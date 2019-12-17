158236ac104e209959213c01d63e079d6605ba3b
cmlee <bal.dongdongdong@gmail.com>
2019-12-18 00:40:54 +0900

    entity isNew

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -36,7 +36,7 @@ public class MemberController {
 				.map(MemberDTO::new);
 	}
 
-	@PostConstruct
+//	@PostConstruct
 	public void init() {
 		for (int i = 0; i < 100; i++) {
 			memberRepository.save(new Member("user" + i, i));
